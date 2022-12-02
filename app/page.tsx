1b7f30b472b58676b2af2af4562d98b30a8fe6ea
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2022-12-02 13:07:35 +0100

    Simulate request

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 function getRuntime() {
-  if ('Bun' in globalThis) return 'bun'
-  if ('Deno' in globalThis) return 'deno'
-  if (globalThis.process?.versions?.node) return 'node'
+  const now = Date.now()
+  if ('Bun' in globalThis) return `bun ${now}`
+  if ('Deno' in globalThis) return `deno ${now}`
+  if (globalThis.process?.versions?.node) return `node ${now}`
+  else return `other ${now}`
 }
 
-export default function Page() {
+async function getData() {
+  const res = Promise.resolve(getRuntime())
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  return res
+}
+export default async function Page() {
+  const data = await getData()
   return (
     <>
       <h1>Hello, Next.js!</h1>
-      <p>Hi from {getRuntime()}</p>
+      <p>Hi from {data}</p>
       <Link href={'about'}>About</Link>
     </>
   )
