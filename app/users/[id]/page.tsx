8fc4b46e3da14246b78eb350f39d1e8b7b358975
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2022-12-03 13:22:48 +0100

    Revert "debug id error"
    This reverts commit 9928ee6c0421912ef8f3dadb4e8d4eb1dadcf618.

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -3,13 +3,13 @@ import { getUser } from '../fetch-users'
 export const runtime = 'experimental-edge'
 
 const Page = async ({ params }: { params: { id: string } }) => {
+  console.log('params', params)
 
-  // const { data } = await getUser(params.id)
+  const { data } = await getUser(params.id)
 
   return (
     <>
-      <p>{JSON.stringify(params)}</p>
-      {/* <h1>
+      <h1>
         {data.first_name} {data.last_name}
       </h1>
       <Image
@@ -17,7 +17,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
         alt={`${data.first_name} ${data.last_name}`}
         width={100}
         height={100}
-      /> */}
+      />
     </>
   )
 }
